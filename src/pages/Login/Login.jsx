e350bc70b25e81ac19c87cb69fcaf345ec2e3bb2
agne-zavackyte <a.zavackyte@gmail.com>
2020-12-10 20:57:15 +0200

    POST added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { Header, FormTemplate, Section } from "../../components";
 import { userData } from "../../utils/data";
+import { AuthContext } from "../../context/AuthContext";
+import { useHistory } from "react-router-dom";
 import * as S from "./Login.style";
 
+function login(data, setError, history, auth) {
+  console.log(data);
+  fetch("http://localhost:8080/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.token) {
+        auth.updateToken(data.token);
+        history.push("/");
+      } else {
+        return setError(data.msg || "Error");
+      }
+    })
+    .catch((error) => setError(error.message));
+}
+
 function Login() {
+  const auth = useContext(AuthContext);
+  const history = useHistory();
+  const [error, setError] = useState("");
+
   return (
     <>
+      {error}
       <S.Hero>
         <Header />
         <Section>
@@ -14,7 +42,9 @@ function Login() {
             type="submit"
             mainBtn="login"
             secBtn="don't have an account"
-            callback={() => alert("Yay")}
+            callback={(fieldValues) =>
+              login(fieldValues, setError, history, auth)
+            }
           />
         </Section>
       </S.Hero>
