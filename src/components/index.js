1a6be47ec36a0f0e3a03a096565ca014b2cd244a
agne-zavackyte <a.zavackyte@gmail.com>
2020-12-11 23:36:20 +0200

    updated with new component

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ export { default as Header } from "./Header/Header";
 export { default as Hero } from "./Hero/Hero";
 export { default as InputField } from "./InputField/InputField";
 export { default as Loading } from "./Loading/Loading";
+export { default as Notification } from "./Notification/Notification";
 export { default as PrivateRoute } from "./PrivateRoute/PrivateRoute";
 export { default as Section } from "./Section/Section";
 export { default as StyledSection } from "./StyledSection/StyledSection";
