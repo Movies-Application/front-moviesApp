9d2f4af0b9d5c6e41111db8a71481dbf006a40f9
agne-zavackyte <a.zavackyte@gmail.com>
2020-12-11 20:38:34 +0200

    components index.js file updated

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
 export { default as Button } from "./Button/Button";
+export { default as Circle } from "./Circle/Circle";
 export { default as FormTemplate } from "./FormTemplate/FormTemplate";
 export { default as Header } from "./Header/Header";
-export { default as Hexagon } from "./Hexagon/Hexagon";
-export { default as Honeycomb } from "./Honeycomb/Honeycomb";
 export { default as InputField } from "./InputField/InputField";
 export { default as Loading } from "./Loading/Loading";
 export { default as Section } from "./Section/Section";
+export { default as StyledSection } from "./StyledSection/StyledSection";
