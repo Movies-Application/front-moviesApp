05645f9a7c4dee08eb13e846f29f750e6cadc6f6
agne-zavackyte <a.zavackyte@gmail.com>
2020-12-12 19:52:25 +0200

    select option style

--- src/components/SelectOption/SelectOption.style.js
+++ src/components/SelectOption/SelectOption.style.js
@@ -7,9 +7,9 @@ export const OptionWrapper = styled.div`
   padding: 0;
   margin: 0;
   padding: 10px 20px;
-  background: ${(props) => props.theme.color.black}; // plus
+  background: ${(props) => props.theme.color.black};
   box-sizing: border-box;
-  color: ${(props) => props.theme.color.white}; // plus
+  color: ${(props) => props.theme.color.white};
   &:not(:last-child) {
     border-bottom: 1px solid rgb(251, 251, 251, 0.2);
   }
