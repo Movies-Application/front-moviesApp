278d7d242d11125f9c31fb0ef7fa3e84e3454bdc
agne-zavackyte <a.zavackyte@gmail.com>
2020-12-14 22:44:05 +0200

    section style color added

--- src/components/Section/Section.style.js
+++ src/components/Section/Section.style.js
@@ -2,11 +2,14 @@ import styled from "styled-components";
 
 export const Section = styled.section`
   background: ${(props) => props.background};
-  padding: 0 2em;
+  color: ${(props) => props.theme.color.white};
+  padding: 0 1em;
+  box-sizing: border-box;
 `;
 
 export const Container = styled.div`
   margin: 0 auto;
   max-width: 768px;
   padding: 30px 20px;
+  box-sizing: border-box;
 `;
