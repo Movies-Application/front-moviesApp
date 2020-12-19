53b93d0fab7709d3f07ec6411f7ef81464217be4
agne-zavackyte <a.zavackyte@gmail.com>
2020-12-19 23:59:16 +0200

    Hero component update

--- src/components/Hero/Hero.style.js
+++ src/components/Hero/Hero.style.js
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 
 export const Hero = styled.div`
-  height: ${(props) => (props.size === "small" ? "80vh" : "100vh")};
+  min-height: ${(props) => (props.size === "cover" ? "100%" : "100vh")};
   margin-top: -182px;
   padding-top: 182px;
+  padding-bottom: 182px;
   background: ${(props) => props.image && `url(${props.image})`};
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
+  background-attachment: fixed;
   box-shadow: ${(props) =>
-    props.shadow ? "inset 0 0 0 2000px rgba(0, 0, 0, 0.8)" : "none"};
+    props.shadow ? "inset 0 0 0 2000px rgba(0, 0, 0, 0.7)" : "none"};
   box-sizing: border-box;
 `;
