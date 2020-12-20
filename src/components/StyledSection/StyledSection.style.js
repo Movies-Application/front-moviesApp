3d0a03aa51258f3dc72825a131b5547471712d01
agne-zavackyte <a.zavackyte@gmail.com>
2020-12-20 17:54:05 +0200

    StyledSection component update

--- src/components/StyledSection/StyledSection.style.js
+++ src/components/StyledSection/StyledSection.style.js
@@ -28,14 +28,26 @@ export const StyledSection = styled.div`
     border-right: 1px solid ${(props) => props.theme.color.white};
     border-bottom: 1px solid ${(props) => props.theme.color.white};
   }
+  background-color: rgb(0, 0, 0, 0.3);
+  box-shadow: 0px 1px 3px 3px rgb(0, 0, 0, 0.4);
 `;
 
 export const Heading = styled.h1`
-  max-width: 70%;
+  max-width: 80%;
   margin: 0;
   color: white;
   text-transform: uppercase;
-  font-size: 2.5em;
+  font-size: 2.3em;
+  opacity: 0.9;
+
+  @media (max-width: 600px) {
+    font-size: 2em;
+  }
+
+  @media (max-width: 450px) {
+    width: 100%;
+    font-size: 1.3em;
+  }
 `;
 
 export const Description = styled.p`
