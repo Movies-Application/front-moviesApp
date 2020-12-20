9e289a0ee08ac78f28dff765af2baf8f20a614f8
agne-zavackyte <a.zavackyte@gmail.com>
2020-12-20 20:55:08 +0200

    pages index.js update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
-export { default as About } from "./About/About";
+export { default as AboutCollection } from "./AboutCollection/AboutCollection";
+export { default as AboutWatchlist } from "./AboutWatchlist/AboutWatchlist";
+export { default as Collection } from "./Collection/Collection";
 export { default as Home } from "./Home/Home";
 export { default as Login } from "./Login/Login";
+export { default as Register } from "./Register/Register";
+export { default as Search } from "./Search/Search";
+export { default as Watchlist } from "./Watchlist/Watchlist";
